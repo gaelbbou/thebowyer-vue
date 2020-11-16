da0a23ba547d8f91e21dbe425a0da2bba830a83e
Gael <gbouvyer@gmail.com>
2020-11-16 13:40:27 +0100

    tb01

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,28 +6,29 @@
 			<a class="link link-theme" href="https://www.linkedin.com/in/gaelbbou/en" target="_blank">
 				<icon-linkedin class="icon icon-theme"></icon-linkedin>
 			</a>
-			&nbsp;
+
+			<!-- &nbsp;
 			<a class="link link-theme" href="https://dribbble.com/gaelbbou" target="_blank">
 				<icon-dribbble class="icon icon-theme"></icon-dribbble>
 			</a>
 			&nbsp;
 			<a class="link link-theme" href="https://github.com/gaelbbou" target="_blank">
 				<icon-github class="icon icon-theme"></icon-github>
-			</a>
+			</a> -->
 		</div>
 	</footer>
 </template>
 
 <script>
 import IconLinkedin from '../assets/icons/IconLinkedin.vue';
-import IconDribbble from '../assets/icons/IconDribbble.vue';
-import IconGithub from '../assets/icons/IconGithub.vue';
+// import IconDribbble from '../assets/icons/IconDribbble.vue';
+// import IconGithub from '../assets/icons/IconGithub.vue';
 
 export default {
 	components: {
-		'icon-linkedin': IconLinkedin,
-		'icon-dribbble': IconDribbble,
-		'icon-github': IconGithub
+		'icon-linkedin': IconLinkedin
+		// 'icon-dribbble': IconDribbble,
+		// 'icon-github': IconGithub
 	}
 }
 </script>
