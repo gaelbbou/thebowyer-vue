e3c2f2ee55d0c9fc6a9689e3d5b339d7e6a61f6f
Gael <gbouvyer@gmail.com>
2020-12-18 16:10:34 +0100

    tb25

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,20 @@
 <template>
 	<footer>
 		<div class="container-fluid align-center">
-			<h5 class=" display-inline-block">2021 The Bowyer</h5>
-			<!-- <h5 class=" display-inline-block">2020 The Bowyer&nbsp;&nbsp;&nbsp;-</h5> -->
-			<!-- &nbsp;
-			<a class="link link-theme" href="https://www.linkedin.com/in/gaelbbou/en" target="_blank">
+			<h5 class=" display-inline-block">2020 The Bowyer&nbsp;&nbsp;&nbsp;-</h5>
+			&nbsp;
+			<a class="link link-theme" href="https://www.linkedin.com/" target="_blank">
 				<icon-linkedin class="icon icon-theme"></icon-linkedin>
 			</a>
 
 			&nbsp;
-			<a class="link link-theme" href="https://dribbble.com/gaelbbou" target="_blank">
+			<a class="link link-theme" href="https://dribbble.com/" target="_blank">
 				<icon-dribbble class="icon icon-theme"></icon-dribbble>
 			</a>
 			&nbsp;
-			<a class="link link-theme" href="https://github.com/gaelbbou" target="_blank">
+			<a class="link link-theme" href="https://github.com/" target="_blank">
 				<icon-github class="icon icon-theme"></icon-github>
-			</a> -->
+			</a>
 		</div>
 	</footer>
 </template>
