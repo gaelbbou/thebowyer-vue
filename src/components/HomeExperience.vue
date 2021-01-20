48addde2485e7d35ddc48fd20910a5569b382c2d
Gael <gbouvyer@gmail.com>
2021-01-20 15:13:12 +0100

    tb27

--- src/components/HomeExperience.vue
+++ src/components/HomeExperience.vue
@@ -9,25 +9,33 @@
 					class="link link-external"
 					href="https://www.linkedin.com/in/gaelbbou/en"
 					target="_blank"
-				>LinkedIn profile</a> or download my
+					>LinkedIn profile</a
+				>
+				or download my
 				<a
 					v-if="themeLight"
 					class="link link-external"
 					v-bind:href="data.website.pdfResumeLight"
 					target="_blank"
-				>resume in PDF</a>
+					>resume in PDF</a
+				>
 				<a
 					v-else
 					class="link link-external"
 					v-bind:href="data.website.pdfResumeDark"
 					target="_blank"
-				>resume in PDF</a>.
+					>resume in PDF</a
+				>.
 			</p>
 			<br />
 
 			<!-- home-language -->
 			<div class="align-center">
-				<select class="field" style="max-width: 240px" v-model="language">
+				<select
+					class="field"
+					style="max-width: 240px"
+					v-model="language"
+				>
 					<option value="Eng">English</option>
 					<option value="Fra">Français</option>
 					<option value="Esp">Español</option>
@@ -37,12 +45,18 @@
 			<br />
 
 			<!-- home-timeline -->
-			<div class="timeline" v-bind:style="{ backgroundImage: 'url(' + data.website.imgGrey + ')' }">
+			<div
+				class="timeline"
+				v-bind:style="{
+					backgroundImage: 'url(' + data.website.imgGrey + ')',
+				}"
+			>
 				<div class="timeline-photo">
 					<img
 						loading="eager"
-						v-bind:src="data.website.imgHeadshot.path1"
-						v-bind:srcset="data.website.imgHeadshot.path2"
+						v-bind:src="headshotpath"
+						v-on:mouseover="headshotOver"
+						v-on:mouseleave="headshotLeave"
 						width="128"
 						height="128"
 					/>
@@ -57,10 +71,18 @@
 					v-bind:class="experience.classSide"
 					class="setting-shadow--motion"
 				>
-					<h3 v-if="language === 'Eng'">{{ experience.title.eng }}</h3>
-					<h3 v-else-if="language === 'Fra'">{{ experience.title.fra }}</h3>
-					<h3 v-else-if="language === 'Esp'">{{ experience.title.esp }}</h3>
-					<h3 v-else-if="language === 'Chi'">{{ experience.title.chi }}</h3>
+					<h3 v-if="language === 'Eng'">
+						{{ experience.title.eng }}
+					</h3>
+					<h3 v-else-if="language === 'Fra'">
+						{{ experience.title.fra }}
+					</h3>
+					<h3 v-else-if="language === 'Esp'">
+						{{ experience.title.esp }}
+					</h3>
+					<h3 v-else-if="language === 'Chi'">
+						{{ experience.title.chi }}
+					</h3>
 					<h5>{{ experience.date }}</h5>
 					<!-- if there is a link -->
 					<p>
@@ -69,33 +91,66 @@
 							class="link link-external text-bold"
 							v-bind:href="experience.url"
 							target="_blank"
-						>{{ experience.company }}</a>
-						<span v-else class="text-bold">{{ experience.company }}</span>
+							>{{ experience.company }}</a
+						>
+						<span v-else class="text-bold">{{
+							experience.company
+						}}</span>
 						<span>
-							<span v-if="language === 'Eng'">{{ experience.description.eng }}</span>
-							<span v-else-if="language === 'Fra'">{{ experience.description.fra }}</span>
-							<span v-else-if="language === 'Esp'">{{ experience.description.esp }}</span>
-							<span v-else-if="language === 'Chi'">{{ experience.description.chi }}</span>
+							<span v-if="language === 'Eng'">{{
+								experience.description.eng
+							}}</span>
+							<span v-else-if="language === 'Fra'">{{
+								experience.description.fra
+							}}</span>
+							<span v-else-if="language === 'Esp'">{{
+								experience.description.esp
+							}}</span>
+							<span v-else-if="language === 'Chi'">{{
+								experience.description.chi
+							}}</span>
 						</span>
 					</p>
 					<span v-if="language === 'Eng'">
-						<p v-for="task in experience.tasks.eng" v-bind:key="task">{{ task }}</p>
+						<p
+							v-for="task in experience.tasks.eng"
+							v-bind:key="task"
+						>
+							{{ task }}
+						</p>
 					</span>
 					<span v-else-if="language === 'Fra'">
-						<p v-for="task in experience.tasks.fra" v-bind:key="task">{{ task }}</p>
+						<p
+							v-for="task in experience.tasks.fra"
+							v-bind:key="task"
+						>
+							{{ task }}
+						</p>
 					</span>
 					<span v-else-if="language === 'Esp'">
-						<p v-for="task in experience.tasks.esp" v-bind:key="task">{{ task }}</p>
+						<p
+							v-for="task in experience.tasks.esp"
+							v-bind:key="task"
+						>
+							{{ task }}
+						</p>
 					</span>
 					<span v-else-if="language === 'Chi'">
-						<p v-for="task in experience.tasks.chi" v-bind:key="task">{{ task }}</p>
+						<p
+							v-for="task in experience.tasks.chi"
+							v-bind:key="task"
+						>
+							{{ task }}
+						</p>
 					</span>
 					<br />
 					<button
 						v-for="tag in experience.tags"
 						v-bind:key="tag"
 						class="button button-mini button-tag button-base--ghost"
-					>{{ tag }}</button>
+					>
+						{{ tag }}
+					</button>
 				</div>
 				<!-- /vfor -->
 
@@ -107,42 +162,50 @@
 			<br />
 			<div class="subsection">
 				<span v-if="language === 'Eng'">
-					<h3 class="align-center">{{ data.website.about.title.eng }}</h3>
+					<h3 class="align-center">
+						{{ data.website.about.title.eng }}
+					</h3>
 					<p class="align-center">
 						{{ data.website.about.description1.eng }}
-						<br>
+						<br />
 						{{ data.website.about.description2.eng }}
-						<br>
+						<br />
 						{{ data.website.about.description3.eng }}
 					</p>
 				</span>
 				<span v-else-if="language === 'Fra'">
-					<h3 class="align-center">{{ data.website.about.title.fra }}</h3>
+					<h3 class="align-center">
+						{{ data.website.about.title.fra }}
+					</h3>
 					<p class="align-center">
 						{{ data.website.about.description1.fra }}
-						<br>
+						<br />
 						{{ data.website.about.description2.fra }}
-						<br>
+						<br />
 						{{ data.website.about.description3.fra }}
 					</p>
 				</span>
 				<span v-else-if="language === 'Esp'">
-					<h3 class="align-center">{{ data.website.about.title.esp }}</h3>
+					<h3 class="align-center">
+						{{ data.website.about.title.esp }}
+					</h3>
 					<p class="align-center">
 						{{ data.website.about.description1.esp }}
-						<br>
+						<br />
 						{{ data.website.about.description2.esp }}
-						<br>
+						<br />
 						{{ data.website.about.description3.esp }}
 					</p>
 				</span>
 				<span v-else-if="language === 'Chi'">
-					<h3 class="align-center">{{ data.website.about.title.chi }}</h3>
+					<h3 class="align-center">
+						{{ data.website.about.title.chi }}
+					</h3>
 					<p class="align-center">
 						{{ data.website.about.description1.chi }}
-						<br>
+						<br />
 						{{ data.website.about.description2.chi }}
-						<br>
+						<br />
 						{{ data.website.about.description3.chi }}
 					</p>
 				</span>
@@ -159,9 +222,18 @@ export default {
 	data() {
 		return {
 			data: Json,
+			headshotpath: Json.website.imgHeadshot,
 			language: 'Eng'
 		}
 	},
-	mixins: [themeTypeMixin]
+	mixins: [themeTypeMixin],
+	methods: {
+		headshotOver() {
+			this.headshotpath = Json.website.imgHeadshotThug
+		},
+		headshotLeave() {
+			this.headshotpath = Json.website.imgHeadshot
+		}
+	}
 }
 </script>
